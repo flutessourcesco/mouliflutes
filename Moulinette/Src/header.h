e754ddee8c05d006c72d788850a6b7b4c1fc3275
root <root@localhost.localdomain>
2017-08-14 20:34:30 -0700

    Update

--- Moulinette/Src/header.h
+++ Moulinette/Src/header.h
@@ -9,7 +9,7 @@ void ft_putstr(char *str);
 void ft_print_alphabet(void);
 void ft_print_reverse_alphabet(void);
 void ft_print_numbers(void);
-void ft_is_negative(void);
+void ft_is_negative(int);
 void ft_print_comb(void);
 void ft_print_comb2(void);
 void ft_putnbr(int nb);
